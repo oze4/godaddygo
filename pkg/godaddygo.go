e32fdc4315a8e5339b8214bdf2076d3ddadfa27b
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-25 01:31:00 -0500

    Test

---
+++ pkg/godaddygo.go
@@ -0,0 +1,10 @@
+package godaddygo
+
+// import (
+//     "github.com/oze4/godaddygo/internal/pkg/endpoints/domains"
+// )
+
+// HelloFromGoDaddyGo does a thing
+func HelloFromGoDaddyGo() string {
+    return "Is it working?"
+}
