41e1c33d4991f5978f984a97ce4304351bf6eada
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-26 14:08:43 -0500

    Updated

--- godaddygo.go
+++ godaddygo.go
@@ -4,7 +4,7 @@ import (
     godaddy "github.com/oze4/godaddygo/pkg/client"
 )
 
-// NewClient creates a new GoDaddy client
+// NewClient creates a new GoDaddy client.
 func NewClient(apiKey, apiSecret, apiMode string) godaddy.Client {
     opts := godaddy.NewOptions(apiKey, apiSecret, apiMode)
     return godaddy.Client{Options: opts}
