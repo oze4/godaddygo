25dcbdbc15d647ff8746ec6ebc4637037c22fd95
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-26 14:05:49 -0500

    Update

--- godaddygo.go
+++ godaddygo.go
@@ -4,14 +4,8 @@ import (
     godaddy "github.com/oze4/godaddygo/pkg/client"
 )
 
-// TestingFunc is for testing
-//func TestingFunc() {
-//    u := url.NewBuilder().APIV1().Domains().Available("xyz.com")
-//    fmt.Println(u)
-//}
-
 // NewClient creates a new GoDaddy client
-func NewClient() godaddy.Client {
-    opts := godaddy.NewOptions()
+func NewClient(apiKey, apiSecret, apiMode string) godaddy.Client {
+    opts := godaddy.NewOptions(apiKey, apiSecret, apiMode)
     return godaddy.Client{Options: opts}
 }
