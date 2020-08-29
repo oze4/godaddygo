809e28ac9810269fdffdf66d611afb4263229cac
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-29 16:58:11 -0500

    Update request.go

--- pkg/http/request.go
+++ pkg/http/request.go
@@ -1,14 +1,6 @@
 package http
 
 // Request holds request data
-// type Request interface {
-// 	APIKey() string
-// 	APISecret() string
-// 	URL() string
-// 	Method() string
-// }
-
-// Request holds request data
 type Request struct {
 	APIKey    string
 	APISecret string
@@ -24,26 +16,3 @@ func (r *Request) Do() {
 
 	// Need to verify request method!
 }
-
-/*
-func (r *request) APIKey() string {
-	return r.apiKey
-}
-
-func (r *request) APISecret() string {
-	return r.apiSecret
-}
-
-func (r *request) URL() string {
-	return r.url
-}
-
-func (r *request) Method() string {
-	return r.method
-}
-
-// NewRequest returns a new request object to send raw requests to GoDaddy
-func NewRequest() Request {
-	return &request{}
-}
-*/
