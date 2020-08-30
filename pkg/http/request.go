63aefd3eeb2a28ece94893df573587f564c7e682
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-30 14:50:38 -0500

    v0.0.22-alpha

--- pkg/http/request.go
+++ pkg/http/request.go
@@ -3,9 +3,11 @@ package http
 import (
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+
+	//"net/url"
 	"strings"
 
 	"github.com/oze4/godaddygo/internal/validator"
@@ -22,56 +24,69 @@ var RequestMethods = map[string]string{
 
 // Request holds request data
 type Request struct {
-	APIKey    string
+	// GoDaddy API Key, note that the prod and dev API's have unique API keys/secrets
+	APIKey string
+	// GoDaddy API Secret, note that the prod and dev API's have unique API keys/secrets
 	APISecret string
-	Method    string
-	URL       string
-	Host      string
-	Body      []byte
+	// HTTP REST method we validate this
+	Method string
+	// The URL you wish to send your request to
+	URL string
+	// The GoDaddy domain name you wish to target - mostly used internally
+	Host string
+	// The body of your request, if you need one
+	Body []byte
 }
 
 // Do sends the http request
-func (r *Request) Do() (bodyBytes []byte, err error) {
+func (r *Request) Do() ([]byte, error) {
+	// Verify we were given a valid REST method
 	valid := validator.Validate(r.Method, RequestMethods)
 	if valid != true {
 		return nil, errors.New("Invalid request method")
 	}
 
-	parsedURL, err := url.Parse(r.URL)
-	if err != nil {
-		return nil, err
-	}
-
-	req := &http.Request{
-		URL:    parsedURL,
-		Method: r.Method,
-	}
-
-	req.Header.Add("Authorization", r.makeAuthString())
-
+	// Sort out whether or not there is a Body
+	var bodyFin io.ReadCloser
 	if r.Body != nil {
-		req.Body = ioutil.NopCloser(strings.NewReader(string(r.Body)))
+		bodyFin = ioutil.NopCloser(strings.NewReader(string(r.Body)))
 	}
 
-	fmt.Println(req)
-
-	result, err := http.DefaultClient.Do(req)
+	// Create new REST request
+	req, err := http.NewRequest(r.Method, r.URL, bodyFin)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(result.StatusCode)
+	// Add authorization to our request
+	req.Header.Set("Authorization", r.makeAuthString())
 
-	bod, err := ioutil.ReadAll(result.Body)
+	// Create new http client to send our request
+	httpclient := &http.Client{}
+
+	// Send request, check for error
+	resp, err := httpclient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
-	result.Body.Close()
+	// defer resp.Body.Close()
+	fmt.Println(resp.StatusCode)
 
-	return bod, nil
+	// Read response body
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Close the response body
+	resp.Body.Close()
+
+	// Return response body as bytes
+	return result, nil
 }
 
+// makeAuthString is used to generate the required auth string for the GoDaddy API
 func (r *Request) makeAuthString() string {
-	return "sso " + r.APIKey + ":" + r.APISecret
+	return "sso-key " + r.APIKey + ":" + r.APISecret
 }
