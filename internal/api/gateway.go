b6cba3f443a785e10fd669c75697fbc8aeae7e78
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-30 00:39:09 -0500

    v0.0.11-alpha

--- internal/api/gateway.go
+++ internal/api/gateway.go
@@ -16,16 +16,10 @@ type gateway struct {
 
 // V1 returns the V1 section of the GoDaddy API
 func (g *gateway) V1() V1 {
-	g.attach("1")
 	return &v1{g.Request}
 }
 
 // V2 returns the V2 section of the GoDaddy API
 func (g *gateway) V2() {
-	g.attach("2")
 	panic("V2 not implemented yet!")
-}
-
-func (g *gateway) attach(version string) {
-	g.URL = g.URL + "/v" + version
 }
