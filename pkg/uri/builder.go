12b60868be3ea03eeeb97f0a9f6d6f563d3a24fa
Matt Oestreich <matthewpoestreich@gmail.com>
2020-09-10 16:49:22 -0500

    Update builder.go

--- pkg/uri/builder.go
+++ pkg/uri/builder.go
@@ -2,7 +2,7 @@ package uri
 
 // Builder is the start of generating an API URL string
 func Builder(isproduction bool) Gateway {
-	h := "https://api-ote.godaddy.com"
+	h := "https://api.ote-godaddy.com"
 	if isproduction {
 		h = "https://api.godaddy.com"
 	}
