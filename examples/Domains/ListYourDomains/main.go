877877668457709e0464b0d8fb06c987ae3f9878
Matt Oestreich <matthewpoestreich@gmail.com>
2020-09-11 16:33:44 -0500

    add example

---
+++ examples/Domains/ListYourDomains/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/oze4/godaddygo"
+)
+
+func main() {
+	// Your info...
+	prodKey := "-"
+	prodSecret := "-"
+	// Connect to production V1 API
+    api := godaddygo.ConnectProduction(prodKey, prodSecret).V1()
+    // Get all domains you own
+	mydoms, err := api.Domains().My()
+	if err != nil {
+		panic(err.Error())
+    }
+    
+    // Loop through them
+	for _, d := range *mydoms {
+		fmt.Println(d.Domain + " " + d.Status)
+	}
+}
