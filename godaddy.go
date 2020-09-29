93123ccdc4ae5fc13632559d39c02f02fa2f5c03
Matt Oestreich <matthewpoestreich@gmail.com>
2020-09-29 14:06:53 -0500

    testing v1.3.16

--- godaddy.go
+++ godaddy.go
@@ -2,21 +2,39 @@ package godaddygo
 
 import (
 	"net/http"
+	"time"
+)
+
+const (
+	// maxHTTPClientTimeout in seconds is the defalt max http client timeout
+	// https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
+	// 1 minute by default
+	maxHTTPClientTimeout = time.Second * 60
 )
 
 // NewConfig creates a new config
 func NewConfig(key, secret, env string) *Config {
-	return &Config{client: http.DefaultClient, key: key, secret: secret, env: env}
+	client := &http.Client{
+		Timeout: maxHTTPClientTimeout,
+	}
+	return &Config{
+		client: client,
+		key:    key,
+		secret: secret,
+		env:    env,
+	}
 }
 
 // NewProduction targets GoDaddy production API
 func NewProduction(key string, secret string) (API, error) {
-	return newAPI(NewConfig(key, secret, APIProdEnv))
+	c := NewConfig(key, secret, APIProdEnv)
+	return newAPI(c)
 }
 
 // NewDevelopment targets GoDaddy development API
 func NewDevelopment(key string, secret string) (API, error) {
-	return newAPI(NewConfig(key, secret, APIDevEnv))
+	c := NewConfig(key, secret, APIDevEnv)
+	return newAPI(c)
 }
 
 // WithClient returns API using your own `*http.Client`
